de826180eb1ca83b698c86429f2ed47ccb6c849d
koppperqu <83599554+koppperqu@users.noreply.github.com>
2024-01-28 18:40:31 -0600

    Initial commit

---
+++ src/main/java/com/BetterTfrrsWebsite/BetterTfrrsWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.BetterTfrrsWebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterTfrrsWebsiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetterTfrrsWebsiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BetterTfrrsWebsite/BetterTfrrsWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BetterTfrrsWebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterTfrrsWebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
